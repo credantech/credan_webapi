6a20b66465fa5f3c952cccc92e66018355c35bbb
mondz <mond.zhang@credan.com>
2016-11-03 15:20:58 +0800

    update

--- src/main/java/com/credan/webapi/config/jersey/filter/feature/EncryptProcessor.java
+++ src/main/java/com/credan/webapi/config/jersey/filter/feature/EncryptProcessor.java
@@ -18,13 +18,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.alibaba.fastjson.JSONObject;
-import com.credan.webapi.comm.enums.StatusEnum;
 import com.credan.webapi.comm.util.Json;
-import com.credan.webapi.config.jersey.exception.ParamException;
 import com.credan.webapi.core.service.security.SignService;
 import com.google.common.base.Charsets;
-
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * 入参解密处理器
@@ -32,7 +28,6 @@ import lombok.extern.slf4j.Slf4j;
  * @author Mond
  * @version 1.0.0, $Date: 2016年11月2日 下午2:30:15 $
  */
-@Slf4j
 @Component
 public class EncryptProcessor implements ContainerRequestFilter {
 
@@ -41,20 +36,13 @@ public class EncryptProcessor implements ContainerRequestFilter {
 
 	@Override
 	public void filter(ContainerRequestContext arg0) throws IOException {
-		String param = null;
-		ContainerRequest cr = null;
-		try {
-			cr = (ContainerRequest) arg0;
-			cr.bufferEntity();
-			param = cr.readEntity(String.class);
-			JSONObject params = Json.ObjectMapper.fromJson(param, JSONObject.class);
-			params = signService.processInputParams(params);
-			InputStream inputStream = new ByteArrayInputStream(params.toJSONString().getBytes(Charsets.UTF_8));
-			arg0.setEntityStream(inputStream);
-		} catch (Exception e) {
-			log.error(this.getClass().getSimpleName() + ", param : {}", param);
-			throw new ParamException(StatusEnum.PARAM_FORMAT_ERROR);
-		}
+		ContainerRequest cr = (ContainerRequest) arg0;
+		cr.bufferEntity();
+		String param = cr.readEntity(String.class);
+		JSONObject params = Json.ObjectMapper.fromJson(param, JSONObject.class);
+		params = signService.processInputParams(params);
+		InputStream inputStream = new ByteArrayInputStream(params.toJSONString().getBytes(Charsets.UTF_8));
+		arg0.setEntityStream(inputStream);
 	}
 
 }
--- src/main/java/com/credan/webapi/core/service/security/SignService.java
+++ src/main/java/com/credan/webapi/core/service/security/SignService.java
@@ -35,17 +35,6 @@ public class SignService extends AbstractBasicService {
 		String sign = params.getString("sign");
 		String timestamp = params.getString("timestamp");
 		
-		boolean verify = false;
-		try {
-			verify = RSAHelper.verify(data.getBytes(), merchantPublicKey, sign);
-		} catch (Exception e) {
-			log.error("验签异常 ======",e);
-			throw new ParamException(StatusEnum.PARAM_FORMAT_ERROR);
-		}
-		if (!verify) {
-			log.debug("验签失败=========sign:{}",sign);
-			throw new ParamException(StatusEnum.INVALID_SIGN);
-		}
 		Date timeDate = null;
 		try {
 			timeDate = DateHelper.addMinute(DateHelper.parseDate(timestamp, "yyyy-MM-dd HH:mm:ss"), connetTime);
@@ -54,9 +43,21 @@ public class SignService extends AbstractBasicService {
 			throw new ParamException(StatusEnum.PROPERTY_LENGTH_ERROR, "timestamp");
 		}
 		Date curDate = new Date();
-		if (timeDate.compareTo(curDate) > 0) {
+		if (timeDate.compareTo(curDate) < 0) {
 			log.error("请求过期 ======timestamp:{}",timestamp);
 			throw new ParamException(StatusEnum.INVALID);
+		}
+		
+		boolean verify = false;
+		try {
+			verify = RSAHelper.verify(data.getBytes(), merchantPublicKey, sign);
+		} catch (Exception e) {
+			log.error("验签异常 ======",e);
+			throw new ParamException(StatusEnum.INVALID_SIGN);
+		}
+		if (!verify) {
+			log.debug("验签失败=========sign:{}",sign);
+			throw new ParamException(StatusEnum.INVALID_SIGN);
 		}
 
 		String decryptData;
