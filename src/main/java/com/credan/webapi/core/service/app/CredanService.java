1682a44db1b341cb50237d6f81ea399c4306de72
NealRiverZhang <neal.zhang@credan.com>
2016-11-07 15:02:35 +0800

    update

---
+++ src/main/java/com/credan/webapi/core/service/app/CredanService.java
@@ -0,0 +1,20 @@
+package com.credan.webapi.core.service.app;
+
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSONObject;
+import com.credan.webapi.core.service.AbstractBasicService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class CredanService extends AbstractBasicService {
+	
+	
+	public void register(JSONObject jsonObject) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/main/java/com/credan/webapi/resource/app/CredanResource.java
+++ src/main/java/com/credan/webapi/resource/app/CredanResource.java
@@ -6,19 +6,24 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.alibaba.fastjson.JSONObject;
+import com.credan.webapi.core.service.app.CredanService;
 import com.credan.webapi.resource.app.base.BasicResource;
 
 @Path("/v1/zlj")
 public class CredanResource extends BasicResource{
 
-	
+	@Autowired
+	private CredanService credanService;
 	
 	@POST
 	@Path("/register")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response register(String body){
 		JSONObject jsonObject = JSONObject.parseObject(body);
+		credanService.register(jsonObject);
 		return null;
 	}
 	
