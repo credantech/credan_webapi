8ac368ada51f8a2bbd28a61a4ce9311d3bfbcce5
mondz <mond.zhang@credan.com>
2016-11-17 15:54:53 +0800

    remove upload

--- src/main/java/com/credan/webapi/core/dao/mapper/file/FileInfoDao.java
+++
@@ -1,7 +0,0 @@
-package com.credan.webapi.core.dao.mapper.file;
-
-import com.credan.webapi.config.mybatis.conf.MyBatisDao;
-import com.credan.webapi.core.dao.entity.file.FileInfo;
-
-public interface FileInfoDao extends MyBatisDao<FileInfo>{
-}
