b7511e9cc1c2049b1970272ee479149eb2123ddc
NealRiverZhang <neal.zhang@credan.com>
2016-11-18 11:27:09 +0800

    update

---
+++ src/main/java/com/credan/webapi/core/service/api/zlj/MeichaiService.java
@@ -0,0 +1,14 @@
+package com.credan.webapi.core.service.api.zlj;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.credan.webapi.core.service.AbstractBasicService;
+import com.credan.webapi.core.service.merchant.MerchantInfoService;
+
+public class MeichaiService extends AbstractBasicService{
+
+	@Autowired
+	private MerchantInfoService merchantInfoService;
+	
+	
+}
